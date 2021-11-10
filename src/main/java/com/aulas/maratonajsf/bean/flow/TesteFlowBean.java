08b7496de8361a6aed566b26b2ff8265cde0c6b9
Gustavo Tavares <gustavo.tavares24@outlook.com>
2021-11-10 11:57:38 -0300

    Escopos - FlowScoped

---
+++ src/main/java/com/aulas/maratonajsf/bean/flow/TesteFlowBean.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aulas.maratonajsf.bean.flow;
+
+import java.io.Serializable;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Marketing
+ */
+@Named
+@FlowScoped(value = "registration")
+public class TesteFlowBean implements Serializable {
+    private String nome;
+    private String sobrenome;
+    private String endereco;
+
+    public String salvar(){
+        System.out.println("Salvando no banco...");
+        System.out.println(nome);
+        System.out.println(sobrenome);
+        System.out.println(endereco);
+        return "exitToInicio";
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
