0250cf1269212a71260fb826caec3a028617ca32
Gustavo Tavares <gustavo.tavares24@outlook.com>
2021-11-13 10:10:40 -0300

    Ciclo de vida do JSF - Exemplo de funcionamento do immediate=true

--- src/main/java/com/aulas/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
+++ src/main/java/com/aulas/maratonajsf/bean/comunicacao/ComunicacaoTeste12Bean.java
@@ -20,11 +20,27 @@ import javax.inject.Named;
 @RequestScoped
 public class ComunicacaoTeste12Bean implements Serializable{
     private Estudante estudante;
+    private Estudante estudante2 = new Estudante();
     
     public void init(){
         estudante = (Estudante) ApplicationMapUtil.getValueFromApplicationMap("estudante");
+        
+    }
+    
+    public String voltar(){
+        System.out.println(estudante2.getNome());
+        System.out.println(estudante2.getSobrenome());
+        return "comunicacao11?faces-redirect=true";
     }
 
+    public Estudante getEstudante2() {
+        return estudante2;
+    }
+
+    public void setEstudante2(Estudante estudante2) {
+        this.estudante2 = estudante2;
+    }
+    
     public Estudante getEstudante() {
         return estudante;
     }
