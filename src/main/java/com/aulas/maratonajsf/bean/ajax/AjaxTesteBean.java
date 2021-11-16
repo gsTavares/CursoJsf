46146184c07073ef11c88367d4d984572b785946
Gustavo Tavares <gustavo.tavares24@outlook.com>
2021-11-16 10:09:47 -0300

    Ajax parte 1 - Introdução, execute e render

---
+++ src/main/java/com/aulas/maratonajsf/bean/ajax/AjaxTesteBean.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aulas.maratonajsf.bean.ajax;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Marketing
+ */
+@Named
+@ViewScoped
+
+public class AjaxTesteBean implements Serializable{
+    private String nome;
+    private String sobrenome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    public void toUppercase(){
+        this.nome = this.nome.toUpperCase();
+        this.sobrenome = this.sobrenome.toUpperCase();
+    }
+}
