1f57a24db52090b94dc6766fcfda9d33ce89df92
Gustavo Tavares <gustavo.tavares24@outlook.com>
2021-11-16 15:52:41 -0300

    Upload de Arquivos parte 2 - Validações básicas

--- src/main/java/com/aulas/maratonajsf/validators/Validators.java
+++ src/main/java/com/aulas/maratonajsf/validators/Validators.java
@@ -15,25 +15,61 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 
 /**
  *
  * @author Marketing
  */
-
 @Named
 @RequestScoped
-public class Validators implements Serializable{
+public class Validators implements Serializable {
+
     private List<String> emailsList = Arrays.asList("gustavo.tavares@gmail.com", "contato@devdojo.com.br");
     @Inject
     private LoginBean loginBean;
-    
+
     public void validateDuplicatedEmail(FacesContext fc, UIComponent uic, Object t) throws ValidatorException {
         String email = (String) t;
-        if(emailsList.contains(email)){
+        if (emailsList.contains(email)) {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "O email já existe cadastrado", "");
-            
+
             throw new ValidatorException(message);
+        }
+    }
+
+    public void validateFile(FacesContext fc, UIComponent uic, Object t) throws ValidatorException {
+        Part file = (Part) t;
+
+        try {
+
+            validateFileNameLength(file);
+            validateContentType(file);
+            validateFileSize(file);
+
+        } catch (RuntimeException e) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), "");
+            throw new ValidatorException(message);
+        }
+    }
+
+    private void validateFileNameLength(Part file) {
+        if (file.getSubmittedFileName().length() > 10) {
+            throw new RuntimeException("O nome + extensão não pode ser maior do que 10 caracteres");
+        }
+    }
+
+    private void validateContentType(Part file) {
+        String contentType = file.getContentType();
+
+        if (!contentType.equals("image/png") && !contentType.equals("image/jpge")) {
+            throw new RuntimeException("Apenas imagens png e jpge são permitidas");
+        }
+    }
+
+    private void validateFileSize(Part file) {
+        if (file.getSize() > 1048576) {
+            throw new RuntimeException("O arquivo não pode exceder 1Mb");
         }
     }
 }
